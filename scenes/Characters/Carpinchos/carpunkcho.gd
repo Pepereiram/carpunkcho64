f8fe0ba97d17b63e24c56d8427fac06b8ac93ff3
Pepereiram <89109716+Pepereiram@users.noreply.github.com>
2024-09-07 22:58:08 -0400

    FIX various
    Various changes

--- scenes/Characters/Carpinchos/carpunkcho.gd
+++ scenes/Characters/Carpinchos/carpunkcho.gd
@@ -1,8 +1,13 @@
 extends CharacterBody3D
 
-
 const SPEED = 10.0
 const JUMP_VELOCITY = 4.5
+
+# vectores qliaos pal mouse
+var ray_origin = Vector3()
+var ray_target = Vector3()
+
+@onready var camara = $Camera3D
 
 func setup(player_data: Statics.PlayerData) -> void:
 	name = str(player_data.id)
@@ -11,6 +16,24 @@ func setup(player_data: Statics.PlayerData) -> void:
 	
 
 func _physics_process(delta: float) -> void:
+	# obtener posicion mouse
+	var mouse_position = get_viewport().get_mouse_position()
+	# Se define el ray origin
+	ray_origin = $Camera.project_ray_origin(mouse_position)
+	# Se define el ray target
+	ray_target = ray_origin + $Camera.project_ray_normal(mouse_position) *2000
+
+	#var query =  PhysicsRayQueryParameters3D(ray_origin, ray_target)
+	var space_state = get_world_3d().direct_space_state
+	var intersection = space_state.intersect_ray(ray_origin, ray_target)
+
+	if not intersection.empty():
+		print("NOT EMPTY!")
+		var pos = intersection.position
+		var look_at_me = Vector3(pos.x, self.translation.y ,pos.z)
+		self.look_at(look_at_me, Vector3.UP )
+	
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
---
+++ scenes/Characters/Carpinchos/inputSync.gd
@@ -0,0 +1,17 @@
+class_name InputSync
+extends MultiplayerSynchronizer
+
+@export var move_input = 0
+@export var jumping = false
+
+func _physics_process(delta: float) -> void:
+	if not is_multiplayer_authority():
+		return
+	move_input = Input.get_axis("move_left", "move_right")
+	if Input.is_action_just_pressed("jump"):
+		jump.rpc()
+
+
+@rpc("reliable", "call_local")
+func jump() -> void:
+	jumping = true
