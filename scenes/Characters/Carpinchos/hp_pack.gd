632328bdeb6dcaf684c953b3323174d4ac3c194f
Pepereiram <89109716+Pepereiram@users.noreply.github.com>
2024-12-10 02:18:04 -0300

    FIX
    New model for the heal pack, now a burger. New assets

--- scenes/Characters/Carpinchos/hp_pack.gd
+++ scenes/Characters/Carpinchos/hp_pack.gd
@@ -1,12 +1,18 @@
 extends StaticBody3D
 
 @export var HEAL = 15
+@export var rotation_speed = 90.0
+
 @onready var area = $Area3D
+@onready var burger = $burger
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	area.connect("body_entered", Callable(self, "_on_body_entered"))
 
+
+func _process(delta: float) -> void:
+	burger.rotate_y(deg_to_rad(rotation_speed*delta))
 
 func _on_body_entered(body: Node) -> void:
 	if body is Player:  # Verifica si el objeto que entra es un Player
